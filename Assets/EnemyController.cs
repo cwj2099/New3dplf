a1ec483270a20a5ed33612790048338d6259bca5
wuji <wc1629@nyu.edu>
2021-04-05 12:15:16 -0400

    finished

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    CharacterController characterController;
     [SerializeField] private float speed = 4.0f;
     GameObject player;
+
+    private Vector3 moveDirection = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
+        characterController = GetComponent<CharacterController>();
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //Debug.Log(Vector3.Distance(player.transform.position, transform.position));
         if (Vector3.Distance(player.transform.position, transform.position) <= 50)
         {
-
+            moveDirection = (player.transform.position - transform.position).normalized;
         }
+        characterController.Move(moveDirection * speed * Time.deltaTime);
     }
 }
